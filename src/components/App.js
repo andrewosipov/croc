b88f62447d7ab689dab6a21585a6353d72214e91
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 20:19:00 +0500

    remove excess variables

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import Header from './Header';
 import Stepper from "../routes/Stepper";
