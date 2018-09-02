c34a81fdbc0623af4fa56fbd8bd93d410d64d314
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 16:19:49 +0500

    add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends Component {
+    static propTypes = {};
+
+    render() {
+        return (
+            <Button />
+        );
+    }
+}
+
+export default Button;
