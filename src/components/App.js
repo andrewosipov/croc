91c82980390d8fbc3d4548822f9819335b4de024
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 18:12:15 +0500

    remove Stepper in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 import Header from './Header';
-import Stepper from './Stepper';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import NotFound from './NotFound';
@@ -15,7 +14,6 @@ class App extends Component {
         <Router>
             <div className="App">
                 <Header />
-                <Stepper />
                 <Switch>
                     <Route path = '/step1' component={Step1} />
                     <Route path = '/step2' component={Step2} />
