1324bfee034d8dd54a8185d79949ee3dc804a43b
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 22:11:27 +0500

    create success page

---
+++ src/routes/Create.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Create = props => {
+    return (
+        <div>
+            Успешно создана виртуальная машина
+        </div>
+    );
+};
+
+Create.propTypes = {
+
+};
+
+export default Create;
