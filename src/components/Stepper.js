92573614ddcd1e426f4befd20080204a0e6cea92
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 18:07:43 +0500

    remove NavLink in Stepper

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
-import {NavLink} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../styles/Stepper.css';
 
 class Stepper extends Component {
-    static propTypes = {};
+    static propTypes = {
+        activeStep: PropTypes.number
+    };
 
     render() {
+        const {activeStep} = this.props;
         return (
             <ul className="Stepper">
-              <li className="Stepper__list-item"><NavLink to="/step1" className="Stepper__link" activeClassName="Stepper__link--active">1. Образ диска</NavLink></li>
-              <li className="Stepper__list-item"><NavLink to="/step2" className="Stepper__link" activeClassName="Stepper__link--active">2. Параметры</NavLink></li>
+              <li className="Stepper__list-item">1. Образ диска</li>
+              <li className="Stepper__list-item">2. Параметры</li>
             </ul>
         );
     }
