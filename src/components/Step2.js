c2f27a9f0c83edbe5a05ebb0d9b3f5791343d984
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 17:45:41 +0500

    add buttons to Step2

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
+import Button from "./Button";
+
+import '../styles/Step.css';
 
 class Step2 extends Component {
     static propTypes = {};
 
     render() {
         return (
-            <div>
+            <div className="Step Step--two">
                 <div className="form"></div>
-                <div className="buttons"></div>
+                <div className="Button-container">
+                    <Button  />
+                    <Button  />
+                </div>
             </div>
         );
     }
