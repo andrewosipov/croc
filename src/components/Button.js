eb8527444009eb12024f55dce4afac8b4b3096d1
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 17:53:28 +0500

    add propTypes and props to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import '../styles/Button.css';
 
 class Button extends Component {
-    static propTypes = {};
+    static propTypes = {
+        to: PropTypes.string,
+        value: PropTypes.string
+    };
 
     render() {
+        const { to, value='Выбрать' } = this.props;
         return (
-            <button className="Button">Выбрать</button>
+            <Link to={to} className="Button">{value}</Link>
         );
     }
 }
