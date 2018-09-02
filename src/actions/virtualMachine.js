b3e689536664fcb1501c1f62e09f293ad871266d
andrewosipov <tech@andrew-osipov.pro>
2018-09-03 03:07:18 +0500

    add new action configureVirtualMachine

--- src/actions/virtualMachine.js
+++ src/actions/virtualMachine.js
@@ -1,4 +1,12 @@
 export const CREATE_VIRTUAL_MACHINE = 'CREATE_VIRTUAL_MACHINE';
+export const CONFIGURE_VIRTUAL_MACHINE = 'CONFIGURE_VIRTUAL_MACHINE';
+
+export const configureVirtualMachine = (virtualMachine) => ({
+    type: CONFIGURE_VIRTUAL_MACHINE,
+    payload: {
+        virtualMachine
+    }
+});
 
 export const createVirtualMachine = (virtualMachine) => ({
     type: CREATE_VIRTUAL_MACHINE,
--- src/reducers/virtualMachine.js
+++ src/reducers/virtualMachine.js
@@ -1,9 +1,12 @@
-import {CREATE_VIRTUAL_MACHINE} from "../actions/virtualMachine";
+import {CREATE_VIRTUAL_MACHINE, CONFIGURE_VIRTUAL_MACHINE} from "../actions/virtualMachine";
 
 export default (virtualMachineState = '', action) => {
     const {type, payload} = action;
 
     switch (type) {
+        case CONFIGURE_VIRTUAL_MACHINE:
+            return virtualMachineState;
+
         case CREATE_VIRTUAL_MACHINE:
             return virtualMachineState;
     }
