0288643e3cc495bb4208e08aea652fb9281335c6
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 16:11:01 +0500

    add a stepper

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../styles/Stepper.css';
 
 class Stepper extends Component {
     static propTypes = {};
 
     render() {
         return (
-            <ul className="stepper">
-               <li><NavLink to="/step1" activeClassName="active">1. Образ диска</NavLink></li>
-                <li><NavLink to="/step2" activeClassName="active">2. Параметры</NavLink></li>
+            <ul className="Stepper">
+              <li className="Stepper__list-item"><NavLink to="/step1" className="Stepper__link" activeClassName="Stepper__link--active">1. Образ диска</NavLink></li>
+              <li className="Stepper__list-item"><NavLink to="/step2" className="Stepper__link" activeClassName="Stepper__link--active">2. Параметры</NavLink></li>
             </ul>
         );
     }
