06a5d611a5d79c84223d33a938a145ec8c53249a
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 17:31:38 +0500

    update Filter with setState

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,10 +4,20 @@ import PropTypes from 'prop-types';
 class Filter extends Component {
     static propTypes = {};
 
+    state = {
+        search: ''
+    };
+
+    changeHandler = (ev) => {
+        this.setState({
+            search: ev.target.value
+        })
+    };
+
     render() {
         return (
             <div className="Filter">
-
+                <input type="text" className="Filter__search" onChange={this.changeHandler} value={this.state.search} />
             </div>
         );
     }
