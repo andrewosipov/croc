f78e79ee1e44e11b9745edc2ec8637103c869cee
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 16:12:29 +0500

    update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
 import Header from './Header';
 import Stepper from './Stepper';
 import Step1 from './Step1';
 import Step2 from './Step2';
+
 import '../styles/App.css';
 
 class App extends Component {
@@ -11,12 +13,10 @@ class App extends Component {
     return (
         <Router>
           <div className="App">
-
                 <Header />
                 <Stepper />
                 <Step1 />
                 <Step2 />
-
           </div>
         </Router>
     );
