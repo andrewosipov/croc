919268c1d0e6b487ca59246d557da6bc06ec5b4c
andrewosipov <tech@andrew-osipov.pro>
2018-09-02 18:45:31 +0500

    rename Stepper classname

--- src/components/StepperCrumbs.js
+++ src/components/StepperCrumbs.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import '../styles/Stepper.css';
+import '../styles/StepperCrumbs.css';
 
 class StepperCrumbs extends Component {
     static propTypes = {
@@ -9,14 +9,14 @@ class StepperCrumbs extends Component {
 
     render() {
         const {activeStep} = this.props;
-        const activeClassName = 'Stepper__item--active';
+        const activeClassName = 'Stepper-crumbs__item--active';
         const isActiveStep1 = activeStep === 1 ? activeClassName : '';
         const isActiveStep2 = activeStep === 2 ? activeClassName : '';
 
         return (
-            <ul className="Stepper">
-              <li className={`Stepper__item ${isActiveStep1}`}>1. Образ диска</li>
-              <li className={`Stepper__item ${isActiveStep2}`}>2. Параметры</li>
+            <ul className="Stepper-crumbs">
+              <li className={`Stepper-crumbs__item ${isActiveStep1}`}>1. Образ диска</li>
+              <li className={`Stepper-crumbs__item ${isActiveStep2}`}>2. Параметры</li>
             </ul>
         );
     }
