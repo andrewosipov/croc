f1cd28572ed8cf59a2c40e78d1eadc200bd69c20
andrewosipov <tech@andrew-osipov.pro>
2018-09-03 10:08:16 +0500

    add getBool in helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,9 @@
 export function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
+
+export function getBool(value) {
+    let _value=value;
+    if( _value!==true && _value!==false ) _value=false;
+    return _value;
+}
