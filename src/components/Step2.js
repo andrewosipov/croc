ae31683990850cb89a4defeed8c58b617bbd7a2b
andrewosipov <tech@andrew-osipov.pro>
2018-09-03 09:41:59 +0500

    remove unused var

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -9,7 +9,6 @@ class Step2 extends Component {
     static propTypes = {};
 
     render() {
-        const {activeSnapshot} = this.props;
         return (
             <div>
                 <StepperCrumbs activeStep={2} />
