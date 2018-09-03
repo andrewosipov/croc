9d2a2016f70af06b344bc8a98cb22254c5fa193b
andrewosipov <tech@andrew-osipov.pro>
2018-09-03 10:08:56 +0500

    add click handler to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,13 +7,14 @@ import '../styles/Button.css';
 class Button extends Component {
     static propTypes = {
         to: PropTypes.string,
-        value: PropTypes.string
+        value: PropTypes.string,
+        onClick: PropTypes.func
     };
 
     render() {
-        const { to, value='Выбрать' } = this.props;
+        const { to, value='Выбрать', onClick = () => null } = this.props;
         return (
-            <Link to={to} className="Button">{value}</Link>
+            <Link to={to} className="Button" onClick={onClick}>{value}</Link>
         );
     }
 }
